ad44952d004c80c954eed4dccde37fa3ad5719a0
mazn1chenko <maznichenko.144@gmail.com>
2023-06-29 16:46:55 +0300

    Initial Commit

---
+++ WikiCars/ViewControllers/MainPageViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MainPageViewController.swift
+//  WikiCars
+//
+//  Created by m223 on 29.06.2023.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .lightGray
+        title = "WikiCars"
+        
+        setup()
+        layout()
+        
+    }
+    
+    //MARK: - two main function setup and layout
+
+    func setup() {
+        
+        
+    }
+    
+    func layout() {
+        
+        
+    }
+    
+    //MARK: - Fuction like a target for button or some else
+    
+    
+
+}
+
